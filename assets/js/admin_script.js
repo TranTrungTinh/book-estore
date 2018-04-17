934f8fbb460f08d8051b0a0f3bad4c2919ee6764
trhdon123 <trhdon123@gmail.com>
2018-04-18 03:54:07 +0700

    Changes for better reponsiveness

---
+++ assets/js/admin_script.js
@@ -0,0 +1,23 @@
+window.onload = () => {
+    $('#Navigator > .nav > li > a').trigger('click');
+}
+
+$('#Navigator > .nav > li').on('click', () => {
+    if (event.target.tagName === 'A') {
+        let sections = $('#Content > section');
+        for (let i = 0; i < sections.length; i++) {
+            if(('#' + sections[i].getAttribute('id')) === event.target.getAttribute('href')) {
+                sections[i].style.display = 'block';
+                if(window.innerWidth < 761) {
+                    sections[i].style.top = '100px';
+                }
+                else {
+                    sections[i].style.top = '0';
+                }
+            }
+            else {
+                sections[i].style.display = 'none';
+            }
+        }
+    }
+})
