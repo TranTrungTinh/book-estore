5b05a2ddb49805dd98ea3c1d5751d392bc59c5de
Trương Hoàng Duy <trhdon123@gmail.com>
2018-04-19 19:04:13 +0700

    added scripts for changing color based on order's status

--- assets/js/admin_script.js
+++ assets/js/admin_script.js
@@ -1,25 +1,46 @@
+function changeOrderRowStatus(row) {
+    switch (parseInt(row.value)) {
+            case 1:
+                row.closest('tr').className = 'warning';
+                break;
+            case 2:
+                row.closest('tr').className = 'active';
+                break;
+            case 3:
+                row.closest('tr').className = 'success';
+                break;
+            default: // 0
+                row.closest('tr').className = 'danger';
+                break;
+        }
+}
+
 window.onload = () => {
+    // dashboard as default
     $('#Navigator > .nav > li > a').trigger('click');
+    // set color for orders
+    let rows = $('#Orders > table select');
+    $.each(rows, function(index, el) {
+        changeOrderRowStatus(el);
+    });
 }
 
 $('#Navigator > .nav > li').on('click', () => {
     if (event.target.tagName === 'A') {
+        // change section accordingly
         let sections = $('#Content > section');
         for (let i = 0; i < sections.length; i++) {
-            if(('#' + sections[i].getAttribute('id')) === event.target.getAttribute('href')) {
+            if (('#' + sections[i].getAttribute('id')) === event.target.getAttribute('href')) {
                 sections[i].style.display = 'block';
-                if(window.innerWidth < 761) {
-                    sections[i].style.top = '100px';
-                }
-                else {
-                    sections[i].style.top = '0';
-                }
-            }
-            else {
+            } else {
                 sections[i].style.display = 'none';
             }
         }
     }
-    
+
     return false;
-})
+});
+
+$('#Orders > table select').on('change', () => {
+    changeOrderRowStatus(event.target);
+});
