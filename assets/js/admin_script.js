e6e8abd52a003cf908132d6987ed256c4a5ad609
trhdon123 <trhdon123@gmail.com>
2018-04-22 23:57:09 +0700

    seperate code to function for easier management and reusing, add Refresh funtion for lists, add Add New Product funtion, add Remove Existed Product funtion, add Update Existed Product function

--- assets/js/admin_script.js
+++ assets/js/admin_script.js
@@ -1,6 +1,6 @@
-function changeOrderRowStatus(row) {
-    let tr = row.closest('tr')
-    switch (row.value) {
+function changeOrderRowStatus(sel) {
+    let tr = sel.closest('tr');
+    switch (sel.value) {
         case 'pending':
             tr.className = 'warning';
             break;
@@ -10,51 +10,54 @@ function changeOrderRowStatus(row) {
         case 'done':
             tr.className = 'success';
             break;
-        case 'canccel':
+        case 'cancel':
             tr.className = 'danger';
         default:
             break;
     }
 }
 
-window.onload = () => {
-    // dashboard as default
-    $('#Navigator > .nav > li > a:first').trigger('click');
-    // // load products list into table
-    // $('#Products > table > tbody').load('../html/admin_products_list.html');
-    // // load orders list into table
-    // $('#Orders > table > tbody').load('../html/admin_orders_list.html');
-    // set color for order rows based on its status
-    let rows = $('#Orders > table select');
-    if (rows) {
-        $.each(rows, (index, el) => {
-            changeOrderRowStatus(el);
-        });
+function changeAllOrdersStatus() {
+    let sel = $('#Orders table')[0].getElementsByTagName('select');
+    for (let i = 0; i < sel.length; i++) {
+        changeOrderRowStatus(sel[i]);
     }
 }
 
+function loadList(container, url, func) {
+    container.innerHTML = '';
+    container.load(url, func);
+}
+
+function loadListBySection(id) {
+    switch (id) {
+        case 'Products':
+            loadList($('#Products > table > tbody'), '../html/admin_products_list.html');
+            break;
+        case 'Orders':
+            loadList($('#Orders > table > tbody'), '../html/admin_orders_list.html', changeAllOrdersStatus);
+            break;
+        default:
+            break;
+    }
+}
+
+window.onload = () => {
+    // dashboard to load as default
+    $('#Navigator > .nav > li > a:first').click();
+}
+
 $('#Navigator > .nav > li').click((e) => {
-    // change section accordingly
+    // load/reload section accordingly
     e.preventDefault();
     let sections = $('#Content > section');
-    if (sections) {
-        for (let i = 0; i < sections.length; i++) {
-            let sectionId = sections[i].getAttribute('id');
-            if (('#' + sectionId) === e.target.getAttribute('href')) {
-                switch(sectionId) {
-                    case 'Products':
-                    $('#Products > table > tbody').load('../html/admin_products_list.html');
-                    break;
-                    case 'Orders':
-                    $('#Orders > table > tbody').load('../html/admin_orders_list.html');
-                    break;
-                    default:
-                    break;
-                }
-                $('#' + sectionId).show('slide');
-            } else {
-                $('#' + sectionId).hide();
-            }
+    for (let i = 0; i < sections.length; i++) {
+        let sectionId = sections[i].getAttribute('id');
+        if (('#' + sectionId) === e.target.getAttribute('href')) {
+            loadListBySection(sectionId);
+            $('#' + sectionId).show('slide');
+        } else {
+            $('#' + sectionId).hide();
         }
     }
 
@@ -62,27 +65,94 @@ $('#Navigator > .nav > li').click((e) => {
 });
 
 // change order row color when its status changed
-$('#Orders > table select').change((e) => {
+$('#Orders > table').change((e) => {
     e.preventDefault();
     changeOrderRowStatus(e.target);
 });
 
-// toggle collapsing an order row for info details & updating
-var prevSelectedRow;
-$('#Products > table > tbody > tr').click((e) => {
+var selectedItem;
+$('#Products > table > tbody').click((e) => {
     e.preventDefault();
     let tr = e.target.closest('tr');
+    selectedItem = tr;
     // copy current selected row info into modal
-    let modal = $('.modal')[0];
     // product image
-    let img = tr.getElementsByTagName('img')[0];
-    modal.getElementsByTagName('img')[0].src = img.src;
+    $('#DetailModal img')[0].src = tr.getElementsByTagName('img')[0].src;
     // product price, amount, author, type, publisher
-    let input = modal.getElementsByTagName('input');
     let td = tr.getElementsByTagName('td');
-    for (let i = 1; i < input.length; i++) {
-        input[i].value = td[i].innerHTML;
+    for (let i = 1; i < td.length - 1; i++) {
+        $('#DetailModal input')[i].value = td[i].textContent;
     }
     // product description
-    modal.getElementsByTagName('textarea')[0].innerHTML = td[2].innerHTML;
+    $('#DetailModal textarea')[0].value = td[td.length - 1].textContent;
+});
+
+// Clear all field content
+$('#AddNewProduct').click(() => {
+    selectedItem = null;
+    $('#DetailModal img')[0].src = '';
+    $.each($('#DetailModal input'), (index, ele) => {
+        ele.value = '';
+    });
+    $('#DetailModal textarea')[0].value = '';
+})
+
+// Refresh button
+$('.btn-refresh').click((e) => {
+    e.preventDefault();
+    loadListBySection(e.target.closest('section').id);
+})
+
+// Add new product to Products list
+$('#ModalSave').click(() => {
+    let imgSrc,
+        price = $('#DetailModal input')[1].value,
+        amount = $('#DetailModal input')[2].value,
+        author = $('#DetailModal input')[3].value,
+        type = $('#DetailModal input')[4].value,
+        publisher = $('#DetailModal input')[5].value,
+        description = $('#DetailModal textarea')[0].value;
+
+    // add new product
+    if(!selectedItem) {
+        imgSrc = $('#DetailModal input')[0].files[0].name
+        let tr = document.createElement('tr');
+        tr.innerHTML = '<tr data-toggle="modal" data-target="#DetailModal">' +
+            '<td class="img-wrapper img-wrapper-sm">' +
+            '<img src="' + imgSrc + '" alt="Ảnh minh họa">' +
+            '</td>' +
+            '<td>' + price + '</td>' +
+            '<td>' + amount + '</td>' +
+            '<td>' + author + '</td>' +
+            '<td>' + type + '</td>' +
+            '<td>' + publisher + '</td>' +
+            '<td>' +
+            '<div class="detail-wrapper">' +
+            description +
+            '</div>' +
+            '</td>' +
+            '</tr>';
+            
+        $('#Products > table > tbody')[0].appendChild(tr);
+    }
+    // update product info
+    else {
+        imgSrc = $('#DetailModal img')[0].src || $('#DetailModal input')[0].files[0].name;
+        console.log(description);
+        let tds = selectedItem.getElementsByTagName('td');
+        tds[0].getElementsByTagName('img')[0].src = imgSrc;
+        tds[1].textContent = price;
+        tds[2].textContent = amount;
+        tds[3].textContent = author;
+        tds[4].textContent = type;
+        tds[5].textContent = publisher;
+        tds[6].getElementsByClassName('detail-wrapper')[0].innerHTML = description;
+    }
+});
+
+// Delete seelected Products
+$('#ModalDelete').click(() => {
+    if (selectedItem) {
+        $('#Products > table > tbody')[0].removeChild(selectedItem);
+    }
 });
