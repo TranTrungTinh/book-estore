0b2319997fadfe3f0f2a9bd486ba52577ce9b5ab
Tran Trung Tinh <trantrungtinh4954@gmail.com>
2018-05-12 12:47:04 +0700

    handle logout

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,3 +1,4 @@
+/* ============ Function ============*/
 // setObject LocalStorage
 Storage.prototype.setObject = function(key, value) {
   this.setItem(key, JSON.stringify(value));
@@ -5,15 +6,37 @@ Storage.prototype.setObject = function(key, value) {
 Storage.prototype.getObject = function(key) {
   return JSON.parse(this.getItem(key));
 }
+function localSaveObject(key, obj) {
+  if (localStorage.getItem(key)) 
+    localStorage.removeItem(key);
+  localStorage.setObject(key, obj);
+}
 
-// 
+function localSaveItem(key, value) {
+  if (localStorage.getItem(key)) 
+    localStorage.removeItem(key);
+  localStorage.setItem(key, value);
+}
+/* ============ Function ============*/
+
+
+/* ============ Toggle Menu Tab ============*/
+$('#collapseListGroupHeadingVN').click(() => {
+  $('#collapseListGroupVN').collapse('toggle');
+});
+$('#collapseListGroupHeadingNXB').click(() => {
+  $('#collapseListGroupNXB').collapse('toggle');
+});
+$('#collapseListGroupHeadingTG').click(() => {
+  $('#collapseListGroupTG').collapse('toggle');
+});
+/* ============ Toggle Menu Tab ============*/
+
+
+// Handle list group item
 $('#menu-list').on('click', '.list-group-item' , e => {
   const title = e.currentTarget.firstElementChild.firstChild.nodeValue;
-
-  if (localStorage.getItem('titleList')) 
-    localStorage.removeItem('titleList');
-  localStorage.setItem('titleList', title);
-
+  localSaveItem('titleList', title);
 });
 
 $('#main-content').on('click', '.thumbnail > img', e => {
@@ -28,21 +51,51 @@ $('#main-content').on('click', '.thumbnail > img', e => {
   // if (localStorage.getItem('book')) 
   //   localStorage.removeItem('book');
   // localStorage.setObject('book', book);
-  window.location.href = 'http://localhost:3000/book';
+  window.location.href = '/book';
 });
 
 // Shopping cart button
 $(document).on('click', '#btn-shopping-cart' , e => {
-  window.location.href = 'http://localhost:3000/shopping-cart';
+  window.location.href = '/shopping-cart';
 });
-// Handle login
+
+//Handle account btn
+$('#btn-account').click(e => {
+  const name = localStorage.getItem('account') || '';
+  if (!name) return $('#myModelLogin').modal('show');
+  $('#toggle-account').text(name);
+  window.location.href = '/account';
+});
+
+
+/* ============ Handle Login - Logout ============*/
 $('#btn-signin').click(e => {
   e.preventDefault();
   const email = $('#inputEmailLogin').val();
   const password = $('#inputPasswordLogin').val();
   $.post('/signin', {email, password}, data => {
-    if(data.success) return window.location.href = 'http://localhost:3000/account';
-    swal("Có lỗi xảy ra!", "Sai email hay password", "error");
+    if(!data.success) return swal("Loi!", "Sai email hay password", "error");
+    localStorage.setItem('account', 'Trần Trung Tính');
+    window.location.href = '/account';
   });
   // console.log({email, password});
 });
+$('#btn-logout').click(e => {
+  e.preventDefault();
+  const name = localStorage.getItem('account') || '';
+  if(!name) return;
+  swal({
+    title: "Đăng xuất?",
+    text: "Đăng xuất khỏi tài khoản " + name,
+    icon: "warning",
+    buttons: true,
+    dangerMode: true,
+  })
+  .then(yes => {
+    if(yes) {
+      localStorage.removeItem('account');
+      window.location.href = '/'
+    }
+  });
+});
+/* ============ Handle Login - Logout ============*/
