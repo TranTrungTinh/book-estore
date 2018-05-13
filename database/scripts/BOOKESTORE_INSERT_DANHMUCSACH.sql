0a9a75ae6c832dc8afb68adf8f539286a5019c48
trhdon123 <trhdon123@gmail.com>
2018-05-13 08:46:48 +0700

    Added database (book images & mysql scripts)

---
+++ database/scripts/BOOKESTORE_INSERT_DANHMUCSACH.sql
@@ -0,0 +1,7 @@
+USE BookEStore;
+INSERT INTO DANHMUCSACH VALUES ('01',N'Sách khoa học - công nghệ');
+INSERT INTO DANHMUCSACH VALUES ('02',N'Sách văn học - tiểu thuyết');
+INSERT INTO DANHMUCSACH VALUES ('03',N'Sách kinh tế');
+INSERT INTO DANHMUCSACH VALUES ('04',N'Sách thiếu nhi');
+INSERT INTO DANHMUCSACH VALUES ('05',N'Sách thường Thức - đời Sống');
+INSERT INTO DANHMUCSACH VALUES ('06',N'Sách tham khảo');
