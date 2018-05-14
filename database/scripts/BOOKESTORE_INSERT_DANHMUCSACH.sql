66bcabffea4fbbd89cf26d8f598ed837dfdca131
trhdon123 <trhdon123@gmail.com>
2018-05-14 23:09:45 +0700

    Added NHAXUATBAN tables, fixed syntax issues

--- database/scripts/BOOKESTORE_INSERT_DANHMUCSACH.sql
+++ database/scripts/BOOKESTORE_INSERT_DANHMUCSACH.sql
@@ -1,7 +1,7 @@
 USE BookEStore;
-INSERT INTO DANHMUCSACH VALUES ('01',N'Sách khoa học - công nghệ');
-INSERT INTO DANHMUCSACH VALUES ('02',N'Sách văn học - tiểu thuyết');
-INSERT INTO DANHMUCSACH VALUES ('03',N'Sách kinh tế');
-INSERT INTO DANHMUCSACH VALUES ('04',N'Sách thiếu nhi');
-INSERT INTO DANHMUCSACH VALUES ('05',N'Sách thường Thức - đời Sống');
-INSERT INTO DANHMUCSACH VALUES ('06',N'Sách tham khảo');
+INSERT INTO DANHMUCSACH VALUES ('01','Sách khoa học - công nghệ');
+INSERT INTO DANHMUCSACH VALUES ('02','Sách văn học - tiểu thuyết');
+INSERT INTO DANHMUCSACH VALUES ('03','Sách kinh tế');
+INSERT INTO DANHMUCSACH VALUES ('04','Sách thiếu nhi');
+INSERT INTO DANHMUCSACH VALUES ('05','Sách thường Thức - đời Sống');
+INSERT INTO DANHMUCSACH VALUES ('06','Sách tham khảo');
